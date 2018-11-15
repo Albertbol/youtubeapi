91cc0f1dc3a0a6713ad325556f37c5a8dd302863
Albertbol <albertb@inbox.lv>
2018-11-15 22:44:58 +0100

    a

--- main.js
+++ main.js
@@ -98,14 +98,12 @@
           part: 'snippet,contentDetails,statistics',
           forUsername: channel
         }).then(response =>{
-            console.log(response);
-            const x = channel.statistics.subscriberCount;
             const channel = response.result.items[0];
             const output = `
             <ul class="collection">
                 <li class="collection-item">Title: ${channel.snippet.title}</li>
                 <li class="collection-item">ID: ${channel.id}</li>
-                <li class="collection-item">Subscribers: ${x.toLocaleString()}</li>
+                <li class="collection-item">Subscribers: ${numberWithCommas(channel.statistics.subscriberCount)}</li>
                 <li class="collection-item">Views: ${numberWithCommas(channel.statistics.viewCount)}</li>
                 <li class="collection-item">Videos: ${numberWithCommas(channel.statistics.videoCount)}</li>
             </ul>
